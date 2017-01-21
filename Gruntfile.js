a7d39f361662f95b0137deecbfe3f4d558d9bbd3
Michael Molisani <molisani@seas.upenn.edu>
2017-01-21 14:15:41 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    ts: {
+      all: {
+        tsconfig: true
+      }
+    },
+    concat: {
+      all: {
+        src: ["HEADER.txt", "partygameshow.d.ts"],
+        dest: "index.d.ts"
+      }
+    },
+    sed: {
+      all: {
+        pattern: "@VERSION",
+        replacement: "<%= pkg.version %>",
+        path: "index.d.ts"
+      }
+    },
+    tslint: {
+      options: {
+        configuration: grunt.file.readJSON("tslint.json")
+      },
+      all: {
+        src: ["partygameshow.d.ts"]
+      }
+    },
+    clean: {
+      tscommand: ["tscommand*.tmp.txt"]
+    }
+  });
+
+  require("load-grunt-tasks")(grunt);
+
+  grunt.registerTask("compile", ["ts:all", "concat:all", "sed:all", "clean:tscommand"]);
+  
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# PartyGame.Show Protocol
+
+This module defines the necessary interfaces for building the core host/client applications for PartyGame.Show as well as a general framework for developing additional games.
+
+This readme will eventually contain instructions for use and directions to examples that use this code. For now, this is just a placeholder.
